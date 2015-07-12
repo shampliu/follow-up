42c5cc7dfcc3dcd94ad57c417ba2871deabad2d1
Kristen <kchui9172@gmail.com>
2015-07-12 04:00:12 -0700

    l

---
+++ public/js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  $('.your-class').slick({
+    autoplay:true,
+    autoplaySpeed:2500,
+    centerMode:true,
+   	variableWidth:true,
+   	pauseOnHover:true,
+   	dots:true,
+  });
+
+});
+
+
+
+		
