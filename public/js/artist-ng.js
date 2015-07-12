dd9a413fd20f81d3d94fa22e5d466f9eac196161
Chang Liu <shampliu@gmail.com>
2015-07-11 23:15:45 -0700

    last fm recs

--- public/js/artist-ng.js
+++ public/js/artist-ng.js
@@ -3,6 +3,10 @@ var app = angular.module('artist', []);
 app.controller('artistController', function($scope, $http) {
     var artist = $scope.artist;
 
+    $scope.toPercent = function(n) {
+        return (Math.floor(n * 100)); 
+    }
+
     $scope.getSimilar = function(artist) {
         var url = "http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=" + $scope.artist + "&api_key=171fb2e2186bd06117e222884676d2b4&format=json"
         $http.get(url)
